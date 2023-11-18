48aef541ff97e9f4bcc6d1f6951bdca6f21e3d7d
Htoo Thet <htoothetnaung@Htoos-MacBook-Pro.local>
2023-11-18 21:20:28 +0630

    Add Flexible Data Structue File

---
+++ Documents/FreeCodeCamp/BasicJs/dataStruct.js
@@ -0,0 +1,25 @@
+// Flexible Data Structure
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Michael",
+      "title" : "Black lives matter",
+      "release_year": 1999,
+      "formats": [
+        "DVD",
+        "MP3",
+        "Gif"
+      ]
+    }
+  ];
